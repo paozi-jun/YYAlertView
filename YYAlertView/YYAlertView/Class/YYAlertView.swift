91f1e7dec4a39f94ac0961b01cf2fdacd8091918
向文品 <243888972@qq.com>
2014-06-27 23:44:12 +0800

    init YYAlertView

---
+++ YYAlertView/YYAlertView/Class/YYAlertView.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class YYAlertView: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
